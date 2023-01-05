ff9bed8ae6d9497d519ca2a5d264bb35785b7efd
Alyssa Barrett <cicibarrett@gmail.com>
2023-01-05 16:55:38 +1300

    added useEffect to app.js for zoho integration

--- test-app-3pm/src/App.js
+++ test-app-3pm/src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import './widgetsdk-min';
 
 function App() {
+
+  useEffect(() => {
+    async function initialise () {
+      const createrPromise= await window.ZOHO.CREATOR.init();
+      console.log(createrPromise)
+      const recordOperations =window.ZOHO.CREATOR.API; // this is the promise
+
+       // and I'm parsing it to my context API object to re-use it for every API call
+       console.log(recordOperations)
+    }
+    initialise ();
+
+
+    
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
